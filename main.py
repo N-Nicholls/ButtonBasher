4aa880632715ce6f7f5ae52db3e2dfbe027cef8c
Noah Nicholls <79185125+N-Nicholls@users.noreply.github.com>
2024-01-27 22:45:09 -0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,133 @@
+# Import the pygame module
+import pygame
+import random
+
+# Import pygame.locals for easier access to key coordinates
+# Updated to conform to flake8 and black standards
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+# Define constants for the screen width and height
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
+# Define a player object by extending pygame.sprite.Sprite
+# The surface drawn on the screen is now an attribute of 'player'
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surf = pygame.Surface((75, 25))
+        self.surf.fill((0, 255, 255))
+        self.rect = self.surf.get_rect()
+
+    def update(self, pressed_keys):
+        if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+            print("up")
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+            print("down")
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
+
+            # Keep player on the screen
+        if self.rect.left < 0:
+            self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH:
+            self.rect.right = SCREEN_WIDTH
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+    def gravity(self):
+        self.rect.move_ip(0, 2)
+
+
+class Enemy(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Enemy, self).__init__()
+        self.surf = pygame.Surface((20, 10))
+        self.surf.fill((255, 255, 255))
+        self.rect = self.surf.get_rect(
+            center=(
+                random.randint(SCREEN_WIDTH + 20, SCREEN_WIDTH + 100),
+                random.randint(0, SCREEN_HEIGHT),
+            )
+        )
+        self.speed = random.randint(5, 20)
+    
+    def update(self):
+        self.rect.move_ip(-self.speed, 0)
+        self.rect.move_ip(0, self.speed/2) # gravity
+        if self.rect.right < 0:
+            self.kill()
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Block, self).__init__()
+        self.surf = pygame.Surface(())
+
+
+
+
+
+# basic variables
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+running = True
+enemies = pygame.sprite.Group() # Note: 
+player = Player()
+
+# events and timers
+ADDENEMY = pygame.USEREVENT + 1
+# pygame.time.set_timer(ADDENEMY, 250)
+
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+
+# Main loop
+while running:
+    # for loop through the event queue
+    for event in pygame.event.get():
+        # Check for KEYDOWN event
+        if event.type == KEYDOWN:
+            # If the Esc key is pressed, then exit the main loop
+            if event.key == K_ESCAPE:
+                running = False
+        # Check for QUIT event. If QUIT, then set running to false.
+        elif event.type == QUIT:
+            running = False
+        elif event.type == ADDENEMY:
+            new_enemy = Enemy()
+            enemies.add(new_enemy)
+            all_sprites.add(new_enemy)
+
+
+    pressed_keys = pygame.key.get_pressed()
+    player.update(pressed_keys)
+
+    player.gravity()
+
+    enemies.update()
+
+    # Fill the screen with black
+    screen.fill((0, 0, 0))
+
+    for entity in all_sprites:
+        screen.blit(entity.surf,entity.rect)
+
+    if pygame.sprite.spritecollideany(player, enemies):
+        player.kill()
+        running = False
+
+    # Update the display
+    pygame.display.flip()
