7d640ebfef9ec809b222122304d7ffe06de96f06
nnicholls0 <write2npn@gmail.com>
2024-01-28 23:28:59 -0800

    Updated collision, should work better for future

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-# Import the pygame module
 import pygame
 import random
-
-# Import pygame.locals for easier access to key coordinates
-# Updated to conform to flake8 and black standards
 from pygame.locals import (
     K_UP,
     K_DOWN,
@@ -14,114 +10,86 @@ from pygame.locals import (
     QUIT,
 )
 
-# Consts
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-
-ACCEL = 1
-MAXVX = 50
-MAXVY = 50
-FRICTION = 0.1
-GRAVITY = .5
 
 class PhysChar(pygame.sprite.Sprite):
 
-    velocityX = 0
-    velocityY = 0
-    velocityXMax = MAXVX
-    velocityYMax = MAXVY
+    maxSpeed = 10
+    speedX = 0
+    speedY = 0
 
-    def __init__(self):
+    def __init__(self, xpos = 0, ypos = 0, width = 25, height = 25, red = 255, green = 255, blue = 255):
         super(PhysChar, self).__init__()
-        self.surf = pygame.Surface((25, 25))
+        self.surf = pygame.Surface((width, height))
         self.surf.fill((0, 255, 255))
-        self.rect = self.surf.get_rect()
+        self.rect = self.surf.get_rect(center = (xpos, ypos))
+
+    def move(self, dx, dy):
+        if dx != 0:
+            self.move_single_axis(dx, 0)
+        if dy != 0:
+            self.move_single_axis(0, dy)
+
+    def move_single_axis(self, dx, dy):
+        
+        # Move the rect
+        self.rect.x += dx
+        self.rect.y += dy
+
+        # If you collide with a wall, move out based on velocity
+        for block in blocks:
+            if self.rect.colliderect(block.rect):
+                if dx > 0: # Moving right; Hit the left side of the wall
+                    self.rect.right = block.rect.left
+                    self.speedX -= 1
+                if dx < 0: # Moving left; Hit the right side of the wall
+                    self.rect.left = block.rect.right
+                    self.speedX += 1
+                if dy > 0: # Moving down; Hit the top side of the wall
+                    self.rect.bottom = block.rect.top
+                    self.speedY -= 1
+                if dy < 0: # Moving up; Hit the bottom side of the wall
+                    self.rect.top = block.rect.bottom
+                    self.speedY += 1
 
     def update(self): # super function w/ pressed_keys for movement
+        self.move(self.speedX, 0)
+        self.move(0, self.speedY)
 
-        # 1st Law of Motion
-        self.rect.move_ip(self.velocityX, self.velocityY)
-        # Gravity, as if you're always holding down
-        if self.velocityY < self.velocityYMax:
-            self.velocityY += GRAVITY
-        # Friction
-        if(self.velocityX > 0):
-            self.velocityX -= FRICTION
-        if(self.velocityX < 0):
-            self.velocityX += FRICTION
-        if(self.velocityY > 0):
-            self.velocityY -= FRICTION
-        if(self.velocityY < 0):
-            self.velocityY += FRICTION
-
-        # Keep char on the screen
-        # If the player is bumping into an edge, will slow movement in that direction twofold
-        # This will be a problem later when there are things to bump into that aren't walls.
-        if self.rect.left < 0:
+        if self.rect.left < 0: # moving left
             self.rect.left = 0
-            self.velocityX += 2*ACCEL
-        if self.rect.right > SCREEN_WIDTH:
+            self.speedX += 1
+        if self.rect.right > SCREEN_WIDTH: # moving right
             self.rect.right = SCREEN_WIDTH
-            self.velocityX -= 2*ACCEL
-        if self.rect.top <= 0:
-            self.velocityY += 2*ACCEL
+            self.speedX -= 1
+        if self.rect.top <= 0: # moving up
             self.rect.top = 0
-        if self.rect.bottom >= SCREEN_HEIGHT:
-            self.velocityY -= 2*ACCEL
+            self.speedY += 1
+        if self.rect.bottom >= SCREEN_HEIGHT: # moving down
             self.rect.bottom = SCREEN_HEIGHT
-
-    def handle_collision(self, group): # can be called in classes where necessary, with whatever group is necessary
-        collided_objects = pygame.sprite.spritecollide(self, group, False)
-        for obj in collided_objects:
-            # Collision response logic here
-            # Example: stop movement on collision
-            if self.velocityX > 0:  # Moving right
-                self.rect.right = obj.rect.left
-            elif self.velocityX < 0:  # Moving left
-                self.rect.left = obj.rect.right
-            if self.velocityY > 0:  # Moving down
-                self.rect.bottom = obj.rect.top
-            elif self.velocityY < 0:  # Moving up
-                self.rect.top = obj.rect.bottom
-            self.velocityX = 0
-            self.velocityY = 0
-
+            self.speedY -= 1
+        
+        print(self.speedX, self.speedY)
 
 class Block(PhysChar): # can be collided with, cannot collide itself (won't move)
-    def __init__(self, X, Y):
-        super().__init__()
-        self.surf = pygame.Surface((25, 25))
-        self.surf.fill((165, 42, 42))
-        self.rect = self.surf.get_rect(center = (X, Y))
+    def __init__(self, xpos, ypos, width, height, red, green, blue):
+        super().__init__(xpos, ypos, width, height, red, green, blue)
 
-# Define a player object by extending pygame.sprite.Sprite
-# The surface drawn on the screen is now an attribute of 'player'
 class Player(PhysChar):
 
     def __init__(self):
-        super().__init__()
-        self.surf = pygame.Surface((25, 25)) # overrides the parent's surf
-        self.surf.fill((0, 255, 255))
-        self.rect = self.surf.get_rect()
+        super().__init__() 
     
-    def update(self, pressed_keys, blocks):
-        # if you're moving and not at max speed, increase speed
-        # note: no checks for capability of movement (collision)
-        # might be a problem at higher speeds
-        if pressed_keys[K_DOWN] and self.velocityY < self.velocityYMax: 
-            print(self.velocityY)
-            self.velocityY += ACCEL
-        if pressed_keys[K_UP] and self.velocityY > -self.velocityYMax:
-            print(self.velocityY)
-            self.velocityY -= ACCEL
-        if pressed_keys[K_LEFT] and self.velocityX > -self.velocityXMax:
-            print(self.velocityX)
-            self.velocityX -= ACCEL
-        if pressed_keys[K_RIGHT] and self.velocityX < self.velocityXMax:
-            print(self.velocityX)
-            self.velocityX += ACCEL
+    def update(self, pressed_keys):
+        if pressed_keys[K_DOWN] and self.speedY < self.maxSpeed:
+            self.speedY += 1
+        if pressed_keys[K_UP] and self.speedY > -self.maxSpeed:
+            self.speedY -= 1
+        if pressed_keys[K_LEFT] and self.speedX > -self.maxSpeed:
+            self.speedX -= 1
+        if pressed_keys[K_RIGHT] and self.speedX < self.maxSpeed:
+            self.speedX += 1
         super().update()
-        self.handle_collision(blocks)
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -142,15 +110,21 @@ class Enemy(pygame.sprite.Sprite):
             self.kill()
 
 
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+
 # basic variables
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
+
+# sprites
 enemies = pygame.sprite.Group()
 blocks = pygame.sprite.Group()
 player = Player()
 
-block = Block(990, 540)
+block = Block(990, 540, 75, 75, 255, 255, 255)
 blocks.add(block)
 
 # events and timers
@@ -160,7 +134,6 @@ ADDENEMY = pygame.USEREVENT + 1
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 all_sprites.add(block)
-
 
 clock = pygame.time.Clock()
 
@@ -183,7 +156,7 @@ while running:
 
 
     pressed_keys = pygame.key.get_pressed()
-    player.update(pressed_keys, blocks)
+    player.update(pressed_keys)
 
     enemies.update()
 
