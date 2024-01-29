804ff2f56319a5a1fb371af21fc2b596b6ca8ca7
N-Nicholls <write2npn@gmail.com>
2024-01-29 12:34:47 -0800

    Updated README, addeed level parser Added friction for objects

--- README.md
+++ README.md
@@ -1,4 +1,49 @@
 # ButtonBasher
-A simple game, also working as a tutorial project on how to work with python and pygame. This is for personal use only and uses this python guide directly as a base:
+Button Basher is a game written in python using pygame, serving as an introduction for
+myself into pygame and a refresher for python. The premise being: you take control of
+a character in a selection of levels and have to activate randomly placed buttons around 
+the map before a timer gets to zero. As time goes on random events happen which make the 
+game harder, such as by adjusting the game rules (gravity, input selection) or by creating
+hazards (enemies, lava, etc). You can also upgrade your character to improve your chances
+of survival.
 
+## Works Used
+
+https://www.pygame.org/project-Rect+Collision+Response-1061-.html
+- I used the rect collision response game to learn how to implement accurate and (hopefully) 
+ performance efficient collision detection. I also borrowed the idea of formatting the level 
+ in an array to speed up level design.
 https://realpython.com/pygame-a-primer/
+- I used this guide as an introduction to pygame. The first iteration of the game was
+ functionally identical to what is in this guide, which defined movement, object + sprite
+ placement, and the general gameplay loop.
+
+## Disclaimer (Surely not real legal protection)
+Note: This project serves only as a personal tutorial and offers no financial gain to myself
+or anyone involved. All respective work used will be obviously documented if used.
+
+
+## Current problems
+Should sprite groups be defined globally or in the main?
+Should objects be added to the sprite group on initialization?
+- How to handle removal from sprite group
+
+## Todo
+level designer (dev only)
+- choose a data type (array?)
+- Gridlike placement (32x18), * 60 for full resolution
+- parser
+blocks
+- Fall-through platforms
+- ladders
+- water
+- doors
+timer
+- reads
+- resets
+- moves
+buttons
+- random placement
+- hitbox
+- directional input
+objects
--- main.py
+++ main.py
@@ -10,6 +10,55 @@ from pygame.locals import (
     QUIT,
 )
 
+# constants
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+FRAMERATE = 30
+
+# level is a list of strings, each string is a row of blocks
+# World is 1920x1080, each block is 30x30, so 64x36 blocks
+level = [
+        "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", # 0
+        "B                                                              B", # 1
+        "B                                                              B", # 2
+        "B                                                              B", # 3
+        "B                                                              B", # 4
+        "B                                                              B", # 5
+        "B                                                              B", # 6
+        "B                                                              B", # 7
+        "B                                                              B", # 8
+        "B                                                              B", # 9
+        "B                                                              B", # 10
+        "B                                                              B", # 11
+        "B                                                              B", # 12
+        "B                                                              B", # 13
+        "B                                                              B", # 14
+        "B                                                              B", # 15
+        "B                                                              B", # 16
+        "B                                P                             B", # 17
+        "B                                                              B", # 18
+        "B           BBBB                                               B", # 19
+        "B                                                              B", # 20
+        "B                              BBBBB                           B", # 21
+        "B                                                              B", # 22
+        "B                                                              B", # 23
+        "B                                                BBBB          B", # 24
+        "B                                                              B", # 25
+        "B                                                              B", # 26
+        "BBBB BBB                           RRRRRRRRRRR                 B", # 27
+        "B                                                              B", # 28
+        "B                                                              B", # 29
+        "B                                                         BBBBBB", # 30
+        "B                  B B                                         B", # 31
+        "B                  B B                                         B", # 32
+        "B                  B B                                         B", # 33
+        "B                                                              B", # 34
+        "BBBBBBBBBSSSIIIIIIISSSBBBBBBBBBBBBBBBBBBBBSSSSSBBBBBBBBBBBBBBBBB", # 35
+        ]
+# level constants
+BLOCKSIZE = 30
+OFFSET = BLOCKSIZE/2
+
 
 class PhysChar(pygame.sprite.Sprite):
 
@@ -17,14 +66,15 @@ class PhysChar(pygame.sprite.Sprite):
     speedX = 0
     speedY = 0
     ON_GROUND = 0 # timer 3 to 0, if 0, then on ground
-    ON_GROUND_FRAMES = 3
+    ON_GROUND_FRAMES = 3 # since it carrys over a bit, you can do long/small jumps
+    friction = 0.95 # constant multiplier, lowers by 5% per frame
 
-    def __init__(self, xpos = 0, ypos = 0, width = 25, height = 25, red = 255, green = 255, blue = 255):
-
+    def __init__(self, xpos = 0, ypos = 0, width = 25, height = 25, fric = 0.95, red = 255, green = 255, blue = 255):
         super(PhysChar, self).__init__()
         self.surf = pygame.Surface((width, height))
-        self.surf.fill((0, 255, 255))
+        self.surf.fill((red, green, blue))
         self.rect = self.surf.get_rect(center = (xpos, ypos))
+        self.friction = fric
 
     def move(self, dx, dy):
         if dx != 0:
@@ -38,10 +88,12 @@ class PhysChar(pygame.sprite.Sprite):
         self.rect.x += dx
         self.rect.y += dy
 
+        # Tag controlling if on ground. Every frame lowers by 1, if 0, then on ground
+        # total frames allowed depends on ON_GROUND_FRAMES
         if self.ON_GROUND > 0:
             self.ON_GROUND -= 1
 
-        # If you collide with a wall, move out based on velocity
+        # collision w/ blocks and walls
         for block in blocks:
             if self.rect.colliderect(block.rect):
                 if dx > 0: # moving right 
@@ -52,13 +104,12 @@ class PhysChar(pygame.sprite.Sprite):
                     self.speedX += 1
                 if dy > 0: # moving down
                     self.rect.bottom = block.rect.top
-                    self.speedY -= 1
-                    self.ON_GROUND = self.ON_GROUND_FRAMES
+                    self.speedY = 0 # stop falling, makes it so you don't bounce
+                    self.speedX *= block.friction # friction
+                    self.ON_GROUND = self.ON_GROUND_FRAMES # reset on ground timer
                 if dy < 0: # moving up
                     self.rect.top = block.rect.bottom
                     self.speedY += 1
-
-        # "friction"
         if self.rect.left < 0: # moving left
             self.rect.left = 0
             self.speedX += 1
@@ -71,8 +122,9 @@ class PhysChar(pygame.sprite.Sprite):
         if self.rect.bottom >= SCREEN_HEIGHT: # moving down
             self.rect.bottom = SCREEN_HEIGHT
             self.speedY -= 1
-            self.ON_GROUND = self.ON_GROUND_FRAMES
+            self.ON_GROUND = self.ON_GROUND_FRAMES # reset on ground timer
 
+    # for debugging, prints at start of a frame, so before movement inputs
     def printStuff(self):
         print("x: " + str(self.rect.x) + " y: " + str(self.rect.y) + " speedX: " + str(self.speedX) + " speedY: " + str(self.speedY) + " onGround: " + str(self.ON_GROUND > 0))
 
@@ -81,31 +133,30 @@ class PhysChar(pygame.sprite.Sprite):
         self.move(self.speedX, 0)
         self.move(0, self.speedY)
         
-
 class Block(PhysChar): # can be collided with, cannot collide itself (won't move)
-    def __init__(self, xpos, ypos, width, height, red, green, blue):
-        super().__init__(xpos, ypos, width, height, red, green, blue)
+    def __init__(self, xpos, ypos, width, height, fric, red, green, blue):
+        super().__init__(xpos, ypos, width, height, fric, red, green, blue)
 
 class Player(PhysChar):
+    GRAVITY = 0.6
 
-    def __init__(self):
-        super().__init__() 
+    def __init__(self, xpos, ypos, width, height, fric, red, green, blue):
+        super().__init__(xpos, ypos, width, height, fric, red, green, blue) 
     
     def update(self, pressed_keys):
         # movement
-        player.printStuff()
+        self.printStuff()
         if pressed_keys[K_DOWN] and self.speedY < self.maxSpeed:
             self.speedY += 1
         if pressed_keys[K_UP] and self.speedY > -self.maxSpeed and self.ON_GROUND > 0:
-            self.speedY -= 20
+            self.speedY -= 10
         if pressed_keys[K_LEFT] and self.speedX > -self.maxSpeed:
             self.speedX -= 1
         if pressed_keys[K_RIGHT] and self.speedX < self.maxSpeed:
             self.speedX += 1
 
-        self.speedY += .3 # gravity
+        self.speedY += self.GRAVITY # gravity
         super().update()
-
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -125,31 +176,46 @@ class Enemy(pygame.sprite.Sprite):
         if self.rect.right < 0:
             self.kill()
 
-
-
-SCREEN_WIDTH = 1920
-SCREEN_HEIGHT = 1080
-
-# basic variables
+# main
+# initialize pygame
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 running = True
 
-# sprites
+all_sprites = pygame.sprite.Group()
 enemies = pygame.sprite.Group()
 blocks = pygame.sprite.Group()
-player = Player()
+Mobs = []
+BlockArr = []
+x = OFFSET # places from the center, so offset by 30 (should be 0)
+y = OFFSET
+for row in level:
+        for col in row:
+            if col == "B": #block
+                BlockArr.append(Block(x, y, BLOCKSIZE, BLOCKSIZE, 0.95, 150, 75, 0)) # 60x60 is size of block
+            if col == "P": # player
+                Mobs.append(Player(x, y, BLOCKSIZE, BLOCKSIZE, 0.95, 0, 255, 255))
+            if col == "R": # Redbull
+                BlockArr.append(Block(x, y, BLOCKSIZE, BLOCKSIZE, 1.05, 255, 0, 0))
+            if col == "S": # Sludge
+                BlockArr.append(Block(x, y, BLOCKSIZE, BLOCKSIZE, 0.85, 0, 255, 0))
+            if col == "I": # Ice
+                BlockArr.append(Block(x, y, BLOCKSIZE, BLOCKSIZE, 1, 0, 0, 255))
+            x += BLOCKSIZE # 60x60 is size of block
+        y += BLOCKSIZE
+        x = OFFSET
 
-block = Block(990, 540, 75, 75, 255, 255, 255)
-blocks.add(block)
+# can't do this in each obj, cuz it can't find the element inside the scope of the obj
+# the plus side is that we can use these arrays for calling the objects in the main loop
+for elements in BlockArr:
+    blocks.add(elements)
+    all_sprites.add(elements)
+for elements in Mobs:
+    all_sprites.add(elements)
 
 # events and timers
 ADDENEMY = pygame.USEREVENT + 1
 # pygame.time.set_timer(ADDENEMY, 250) # Temp removed 
-
-all_sprites = pygame.sprite.Group()
-all_sprites.add(player)
-all_sprites.add(block)
 
 clock = pygame.time.Clock()
 
@@ -172,7 +238,7 @@ while running:
 
 
     pressed_keys = pygame.key.get_pressed()
-    player.update(pressed_keys)
+    Mobs[0].update(pressed_keys)
     enemies.update()
 
     # Fill the screen with black
@@ -181,10 +247,10 @@ while running:
     for entity in all_sprites:
         screen.blit(entity.surf,entity.rect)
 
-    if pygame.sprite.spritecollideany(player, enemies):
-        player.kill()
+    if pygame.sprite.spritecollideany(Mobs[0], enemies):
+        Mobs[0].kill()
         running = False
 
     # Update the display
     pygame.display.flip()
-    clock.tick(30) # framerate, 30 fps
+    clock.tick(FRAMERATE) # framerate, 30 fps
