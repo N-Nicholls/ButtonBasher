ee3faafb7870730a758768bce92b52ca6af21ebf
N-Nicholls <write2npn@gmail.com>
2024-01-30 14:42:48 -0800

    Added ability for blockt to check direction of player on it. Added bounce and conveyor (same item) Changed the way the game does logic for items. Before it would update all dynamic objects manually, now its based on contact w/ player. Not fully implemented

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ FRAMERATE = 30
 
 # level is a list of strings, each string is a row of blocks
 # World is 1920x1080, each block is 30x30, so 64x36 blocks
+LEVEL_CHOICE = 0
 level = [
         ("BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB", # 0
         "B                                                              B", # 1
@@ -53,7 +54,7 @@ level = [
         "B                                                         BBBBBB", # 30
         "B                  B B                                         B", # 31
         "B                  B B                                         B", # 32
-        "B                  B B                                         B", # 33
+        "B                  B B BBBGGGSRJJJJ          ^^^^              B", # 33
         "B                                                              B", # 34
         "BBBBBBBBBSSSIIIIIIISSSBBBBBBBBBBBBBBBBBBBBSSSSSBBBBBBBBBBBBBBBBB",), # 35
 
@@ -61,29 +62,29 @@ level = [
         "J                                                              J",
         "J                                                              J",
         "J                                                              J",
+        "J                                               <              J",
+        "J                                               <              J",
+        "J                                               <              J",
+        "J                       ^                       <              J",
+        "J                       ^                       <              J",
+        "J                       ^                       <              J",
+        "J                       ^      P                <              J",
+        "J                       ^                       <              J",
+        "J                                               <              J",
+        "J                                                              J",
+        "J      >>>>>>>>>>>>                         <<<<<<<<<<<<<      J",
+        "J                           ^^^^^                              J",
+        "J                                                              J",
+        "J                     BBBBBBBBBBBBBBBBBBB                      J",
         "J                                                              J",
         "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                              P                               J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
-        "J                                                              J",
+        "J                                     BBBBB                    J",
         "J                  GGGGGGGGGGGG                                J",
         "J                                                              J",
         "J                                                              J",
         "J                                      GGGGGGG                 J",
         "J                                                     G        J",
-        "J                                                     G        J",
+        "J                        BBBBB                        G        J",
         "J                                                     G        J",
         "J             GGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG        J",
         "J                                                              J",
@@ -109,9 +110,16 @@ class PhysChar(pygame.sprite.Sprite):
     ON_GROUND_FRAMES = 3 # since it carrys over a bit, you can do long/small jumps
     friction = 0.95 # constant multiplier, lowers by 5% per frame
     elasticity = 0 # how much it deflects, 0 is no bounce, 1 is perfect bounce. Higher will add energy
-    passable = 0
+    PASSABLE = 0
     PASSABLE_FRAMES = 15 # how long you can fall through a fallthrough block, roughly 1/2 sec @ 30 fps
-    SPEED_CUTOFF = 0.2
+    JUMP_MULT = 1
+    GRAVITY = 0
+    ON_CONVEYORX = 0
+    ON_CONVEYORY = 0
+
+    effects = [ 
+        ON_GROUND, JUMP_MULT, GRAVITY, ON_CONVEYORX, ON_CONVEYORY,
+    ]
 
     def __init__(self, xpos = 0, ypos = 0, width = BLOCKSIZE, height = BLOCKSIZE, fric = 0.95, elas = 0, red = 255, green = 255, blue = 255):
         super(PhysChar, self).__init__()
@@ -120,6 +128,17 @@ class PhysChar(pygame.sprite.Sprite):
         self.rect = self.surf.get_rect(center = (xpos, ypos))
         self.friction = fric
         self.elasticity = elas
+
+    # "abstract" functions for dynamic objects
+    def onTop(self, pc): # called by block, parameter is player
+        pc.ON_GROUND = pc.ON_GROUND_FRAMES
+        pc.JUMP_MULT = 1 + self.elasticity
+    def onBottom(self, pc):
+        pass
+    def onLeft(self, pc):
+        pass
+    def onRight(self, pc):
+        pass
 
     def move(self, dx, dy):
         if dx != 0:
@@ -137,10 +156,12 @@ class PhysChar(pygame.sprite.Sprite):
         # total frames allowed depends on ON_GROUND_FRAMES
         if self.ON_GROUND > 0:
             self.ON_GROUND -= 1
+        self.ON_CONVEYORX = 0
+        self.ON_CONVEYORY = 0
 
         # collision w/ blocks and walls
         for block in blocks:
-            if block.passable <= 0:
+            if block.PASSABLE <= 0:
                 if self.rect.colliderect(block.rect):
                     avgElas = (self.elasticity*block.elasticity)/2
                     if dx > 0: # moving right 
@@ -148,22 +169,27 @@ class PhysChar(pygame.sprite.Sprite):
                         self.speedX = -self.speedX*avgElas# bounce
                         if math.fabs(self.speedY) < self.maxSpeed*1.5: # only lets you get 1.5 times speed
                             self.speedY *= block.friction # friction
+                        block.onRight(self)
                     if dx < 0: # moving left
                         self.rect.left = block.rect.right
                         self.speedX = -self.speedX*avgElas # bounce
                         if math.fabs(self.speedY) < self.maxSpeed*1.5: # only lets you get 1.5 times speed
                             self.speedY *= block.friction # friction
+                        block.onLeft(self)
                     if dy > 0: # moving down
                         self.rect.bottom = block.rect.top
                         self.speedY = -self.speedY*avgElas # stop falling, makes it so you don't bounce
                         if math.fabs(self.speedX) < self.maxSpeed*1.5: # only lets you get 1.5 times speed
                             self.speedX *= block.friction # friction
-                        self.ON_GROUND = self.ON_GROUND_FRAMES # reset on ground timer
+                        block.onTop(self)
                     if dy < 0: # moving up
                         self.rect.top = block.rect.bottom
                         self.speedY = -self.speedY*avgElas # bounce
                         if math.fabs(self.speedX) < self.maxSpeed*1.5: # only lets you get 1.5 times speed
                             self.speedX *= block.friction # friction
+                        block.onBottom(self)
+            else:
+                block.update()
         '''if self.rect.left < 0: # moving left
             self.rect.left = 0
             self.speedX += 1
@@ -187,9 +213,9 @@ class PhysChar(pygame.sprite.Sprite):
         self.move(self.speedX, 0)
         self.move(0, self.speedY)
 
-        if math.fabs(self.speedX) < self.SPEED_CUTOFF: # stop doing stupid calculations
+        if math.fabs(self.speedX) < 0.2: # stop doing stupid calculations
             self.speedX = 0
-        if math.fabs(self.speedY) < self.SPEED_CUTOFF:
+        if math.fabs(self.speedY) < 0.2:
             self.speedY = 0
         
 class Block(PhysChar): # can be collided with, cannot collide itself (won't move)
@@ -200,32 +226,125 @@ class FallThrough(Block): # if you're on it and press down, you fall through
     def __init__(self, xpos, ypos, width = BLOCKSIZE, height = BLOCKSIZE/2, fric = 0.95, elas = 0, red = 0, green = 100, blue = 0):
         super().__init__(xpos, ypos - height/2-1, width, height, fric, elas, red, green, blue)
 
-    def update(self, pressed_keys, pc): # This makes ALL fallthrough blocks fallthrough, not just the one you're on
-        if pressed_keys[K_DOWN] and pc.ON_GROUND > 0: # will need a diff passable flag for enemies + mulitplayer
-            self.passable = self.PASSABLE_FRAMES
+    def onTop(self, pc):
+        pc.ON_GROUND = pc.ON_GROUND_FRAMES
+        pc.JUMP_MULT = 1 + self.elasticity
+        if pc.controls[K_DOWN] and pc.ON_GROUND > 0:
+            self.PASSABLE = self.PASSABLE_FRAMES
         else:
-            if self.passable != 0:
-                self.passable -= 1
+            if self.PASSABLE != 0:
+                self.PASSABLE -= 1
+
+    def update(self):
+        self.PASSABLE -= 1
+
+    '''def update(self, pressed_keys, pc): # This makes ALL fallthrough blocks fallthrough, not just the one you're on
+        if pressed_keys[K_DOWN] and pc.ON_GROUND > 0: # will need a diff passable flag for enemies + mulitplayer
+            self.PASSABLE = self.PASSABLE_FRAMES
+        else:
+            if self.PASSABLE != 0:
+                self.PASSABLE -= 1'''
+
+# Jump Pad applies constant speed for as long as you're on it, which isn't how they actually
+# behave but it also doesn't take away from the game
+class Conveyor(Block): #1-left, 2-right, 3-up, 4-down
+    direction = 0
+    speed = 0
+    def __init__(self, xpos, ypos, direct = 0, speedCon = 5):
+        self.direction = direct
+        self.speed = speedCon
+        super().__init__(xpos, ypos, width = BLOCKSIZE, height = BLOCKSIZE, fric = 0.92, elas = 0, red = 100, green = 100, blue = 0)
+
+    def onTop(self, pc):
+        pc.ON_GROUND = pc.ON_GROUND_FRAMES
+        pc.JUMP_MULT = 1 + self.elasticity
+        match self.direction:
+            case 1:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5: # I feel like theres a better way to do these if
+                    pc.ON_CONVEYORX += -self.speed # prevents gaining too much speed
+            case 2:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORX += self.speed 
+            case 3:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += -self.speed
+            case 4:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += self.speed
+            case _:
+                pass
+
+    def onLeft(self, pc):
+        match self.direction:
+            case 1:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5: 
+                    pc.ON_CONVEYORX += -self.speed 
+            case 2:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORX += self.speed 
+            case 3:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += -self.speed
+            case 4:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += self.speed
+            case _:
+                pass
+    def onRight(self, pc):
+        match self.direction:
+            case 1:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5: 
+                    pc.ON_CONVEYORX += -self.speed 
+            case 2:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORX += self.speed 
+            case 3:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += -self.speed
+            case 4:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += self.speed
+            case _:
+                pass
+    def onBottom(self, pc): # For Mr. Dixon, PhD
+        match self.direction:
+            case 1:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5: 
+                    pc.ON_CONVEYORX += -self.speed 
+            case 2:
+                if math.fabs(pc.speedX) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORX += self.speed 
+            case 3:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += -self.speed
+            case 4:
+                if math.fabs(pc.speedY) < pc.maxSpeed*1.5:
+                    pc.ON_CONVEYORY += self.speed
+            case _:
+                pass
 
 class Player(PhysChar):
-    GRAVITY = 0.6
+    controls = []
 
     def __init__(self, xpos, ypos, width = BLOCKSIZE, height = BLOCKSIZE, fric = 0.95, elas = 1, red = 0, green = 255, blue = 255):
         super().__init__(xpos, ypos, width, height, fric, elas, red, green, blue) 
+        self.GRAVITY = 0.6
     
     def update(self, pressed_keys):
         # movement
         self.printStuff()
-        if pressed_keys[K_DOWN] and self.speedY < self.maxSpeed:
+        self.controls = pressed_keys # this might be very stupid, but it means obj can see what the user is pressing
+        if self.controls[K_DOWN] and self.speedY < self.maxSpeed:
             self.speedY += 1
-        if pressed_keys[K_UP] and self.speedY > -self.maxSpeed and self.ON_GROUND > 0:
-            self.speedY -= 10
-        if pressed_keys[K_LEFT] and self.speedX > -self.maxSpeed:
+        if self.controls[K_UP] and self.speedY > -self.maxSpeed and self.ON_GROUND > 0:
+            self.speedY -= math.fabs(10 * self.JUMP_MULT)
+        if self.controls[K_LEFT] and self.speedX > -self.maxSpeed:
             self.speedX -= 1
-        if pressed_keys[K_RIGHT] and self.speedX < self.maxSpeed:
+        if self.controls[K_RIGHT] and self.speedX < self.maxSpeed:
             self.speedX += 1
 
-        self.speedY += self.GRAVITY # gravity
+        self.speedX += self.ON_CONVEYORX
+        self.speedY += self.GRAVITY + self.ON_CONVEYORY # gravity
         super().update()
 
 class Enemy(pygame.sprite.Sprite):
@@ -262,7 +381,7 @@ BlockArr = []
 
 x = OFFSET # places from the center, so offset by half blocksize (effectively x,y = 0)
 y = OFFSET
-for row in level[1]:
+for row in level[LEVEL_CHOICE]:
         for col in row:
             if col == "B": #block
                 BlockArr.append(Block(x, y))
@@ -281,6 +400,14 @@ for row in level[1]:
                 BlockArr.append(Block(x, y, BLOCKSIZE, BLOCKSIZE, 0.95, 1.7, 255, 0, 255))
             if col == "G": # Granite
                 BlockArr.append(Block(x, y, BLOCKSIZE, BLOCKSIZE, 0.93, 0.3, 100, 100, 100))
+            if col == "<": # conveyor left
+                BlockArr.append(Conveyor(x, y, 1, 5)) #1 left, 2 right, 3 up, 4 down
+            if col == ">": # conveyor right
+                BlockArr.append(Conveyor(x, y, 2, 5))
+            if col == "^": # conveyor up
+                BlockArr.append(Conveyor(x, y, 3, 20))
+            if col == "V": #c onveyor down
+                BlockArr.append(Conveyor(x, y, 4, 10))
             x += BLOCKSIZE # 60x60 is size of block
         y += BLOCKSIZE
         x = OFFSET
@@ -318,7 +445,7 @@ while running:
 
 
     pressed_keys = pygame.key.get_pressed()
-    fallThrough.update(pressed_keys, Mobs[0]) # fallthrough update
+    # fallThrough.update(pressed_keys, Mobs[0]) # fallthrough update
     Mobs[0].update(pressed_keys) # player physics update
     enemies.update()
 
